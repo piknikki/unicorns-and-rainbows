df13054245448f67463651e7a15c8ba95aac0f14
Nikki <piknik@me.com>
2021-01-19 16:26:31 -0700

    working on logic for wins

--- assets/data.js
+++ assets/data.js
@@ -1,11 +1,11 @@
 const gameSetup = {
-  r1s1: "",
-  r1s2: "",
-  r1s3: "",
-  r2s1: "",
-  r2s2: "",
-  r2s3: "",
-  r3s1: "",
-  r3s2: "",
-  r3s3: ""
+  r1s1: null,
+  r1s2: null,
+  r1s3: null,
+  r2s1: null,
+  r2s2: null,
+  r2s3: null,
+  r3s1: null,
+  r3s2: null,
+  r3s3: null
 }
--- js/game.js
+++ js/game.js
@@ -15,13 +15,18 @@ class Game {
     // todo --> player makes a choice on the board. Need to know player and board location.
     if (!(square in this.gameBoard)) {
       this.gameBoard[square] = this[player].token
+
     } else if (square in this.gameBoard) {
       this.feedback = 'nope'
     } else if (this.turnCount === 8) {
       this.feedback = 'full'
     }
     this.alternateTurns()
-    this.determineWinner(player)
+
+    if (this.turnCount > 4) {
+      this.determineWinner(player)
+    }
+
   }
 
   // updateBoard() {
@@ -43,11 +48,46 @@ class Game {
     // todo --> check this after every move. check if three in a row in any direction.
     // todo --> make this a switch??
 
+    var response;
     if (this.gameBoard.r1s1 === this.gameBoard.r2s2 && this.gameBoard.r1s1 === this.gameBoard.r3s3) {
-      this.winner = player
-      this.feedback = 'winner'
+      response = 'diagonal left'
+
+    } else if (this.gameBoard.r1s1 === this.gameBoard.r1s2 && this.gameBoard.r1s1 === this.gameBoard.r1s3) {
+      response = 'horizontal top'
+
+    } else if (this.gameBoard.r2s1 === this.gameBoard.r2s2 && this.gameBoard.r2s1 === this.gameBoard.r2s3) {
+      response = 'horizontal middle'
+
+    } else if (this.gameBoard.r3s1 === this.gameBoard.r3s2 && this.gameBoard.r3s1 === this.gameBoard.r3s3) {
+      if (this.gameBoard.r3s1 === undefined || this.gameBoard.r3s2 === undefined || this.gameBoard.r3s3 === undefined) {
+        response = 'none'
+      } else {
+        response = 'horizontal bottom'
+      }
+
+    } else if (this.gameBoard.r1s1 === this.gameBoard.r2s1 && this.gameBoard.r1s1 === this.gameBoard.r3s1) {
+      response = 'vertical left'
+
+    } else if (this.gameBoard.r1s3 === this.gameBoard.r2s3 && this.gameBoard.r1s3 === this.gameBoard.r3s3) {
+      response = 'vertical right'
+
+    } else if (this.gameBoard.r1s2 === this.gameBoard.r2s2 && this.gameBoard.r1s2 === this.gameBoard.r3s2) {
+      response = 'vertical middle'
+
+    } else if (this.gameBoard.r3s1 === this.gameBoard.r2s2 && this.gameBoard.r3s1 === this.gameBoard.r1s3) {
+      response = 'diagonal right'
+
+    } else {
+      response = 'none'
     }
 
+    if (response !== 'none') {
+      console.log(response)
+      this.winner = player
+      this.feedback = 'winner'
+    } else {
+      this.feedback = ''
+    }
   }
 
   determineDraw() {
